93beb7514a2c2278037f79b15d658b09034ebbfc
Kosisochukw <43815706+Kosisochukw@users.noreply.github.com>
2019-02-22 19:17:26 -0500

    Update problem2.cpp

--- lab3/problem2.cpp
+++ lab3/problem2.cpp
@@ -2,4 +2,21 @@
 
 int findElement(int arr[], int ranges[][2], int rotations, int index){
     //write your code here
+ int item;
+for(int i=0;i<rotations;i++)
+{
+int l=ranges[i][0];
+int u=ranges[i][1];
+  
+item=arr[u];
+  
+for(int j=u-1;j>=l;j--)
+{
+arr[j+1]=arr[j];
+}
+arr[l]=item;
+}
+  
+return(arr[index]);
+}   
 }
