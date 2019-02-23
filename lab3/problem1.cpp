57617e79f15e06094ad2a72c8d37f9cd6fbbe8a1
Kosisochukw <43815706+Kosisochukw@users.noreply.github.com>
2019-02-22 19:15:39 -0500

    Update problem1.cpp

--- lab3/problem1.cpp
+++ lab3/problem1.cpp
@@ -2,4 +2,28 @@
 
 void reverseArray(int arr[], int start, int end){
     //write your code here
+ int main()
+{
+
+	int a[] = { 1,2,3 };
+	reverseArray(a, 0, 1); 
+	return 0;
+}
+
+void reverseArray(int arr[], int start, int end)
+{
+	int j = start;
+	int size = (end - start) + 1;
+	int temp[size]; 
+	for (int i = 0; i < size; i++)
+	{
+		temp[i] = arr[j];
+		j++;
+	}
+	for (int i = size - 1, k = start; i >= 0; i--, k++)
+		arr[k] = temp[i];
+	for (int i = 0; i < 3; i++)
+		cout << arr[i] << " ";
+}
+
 }
